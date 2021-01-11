31247bf291c1ba97002922574ce45f44a464478c
BowenGuan <625472429@qq.com>
2021-01-11 10:36:13 +0800

    代码插件构建

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-button>Element按钮</el-button>
+    <div id="main"></div>
   </div>
 </template>
+<script>
+var echarts = require('echarts');
+
+export default {
+  mounted() {
+
+    var myChart = echarts.init(document.getElementById('main'));
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data:['销量']
+      },
+      xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+  }
+}
+</script>
+<style>
+#main{
+  height: 400px;
+  width: 800px;
+}
+</style>
