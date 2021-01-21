a7589cdb126082bee6fe555b5c7bbb0f5704f866
BowenGuan <625472429@qq.com>
2021-01-21 17:28:50 +0800

    完成首页框架编写

---
+++ src/components/home/Probe.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="probe">
+    probe
+  </div>
+</template>
+
+<script>
+export default {
+name: "Probe"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@css/style.scss';
+.probe{
+  font-size: 14px;
+}
+
+
+</style>
