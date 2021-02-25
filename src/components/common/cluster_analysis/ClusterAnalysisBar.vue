0cbf0acef13a916cffba29aed4ef6271658bfc07
BowenGuan <625472429@qq.com>
2021-02-25 14:41:47 +0800

    更新错误信息bar
    更新错误信息bar

--- src/components/common/cluster_analysis/ClusterAnalysisBar.vue
+++ src/components/common/cluster_analysis/ClusterAnalysisBar.vue
@@ -5,6 +5,8 @@
 <script>
 export default {
   name: "ClusterAnalysisBar",
+  //barData: {xData: [], barValue: []}
+  props: ['barData'],
   mounted() {
     let myChart = this.$echarts.init(this.$refs.caBar);
     myChart.setOption( {
@@ -12,7 +14,7 @@ export default {
       grid: {
         containLabel: true,
         top: 40,
-        bottom: 20,
+        bottom: 0,
         right: 10,
         left: 10,
       },
@@ -24,7 +26,8 @@ export default {
       },
       xAxis: {
         type: 'category',
-        data: ['11-01', '11-02','11-03','11-04','11-05','11-06','11-07','11-08','11-09','11-10','11-11','11-12','11-13','11-14','11-15'],
+        data: this.barData.xData,
+        // data: ['11-01', '11-02','11-03','11-04','11-05','11-06','11-07','11-08','11-09','11-10','11-11','11-12','11-13','11-14','11-15'],
         axisTick: {
           lineStyle: {
             color: '#c1c5ca41',
@@ -55,8 +58,8 @@ export default {
         }
       },
       series: [{
-
-        data: [120, 200, 150, 80, 70, 110, 130, 500,100,800,600,150,300,500,180],
+        data: this.barData.barValue,
+        // data: [120, 200, 150, 80, 70, 110, 130, 500,100,800,600,150,300,500,180],
         type: 'bar',
         itemStyle: {
           //柱形图圆角，鼠标移上去效果，如果只是一个数字则说明四个参数全部设置为那么多
